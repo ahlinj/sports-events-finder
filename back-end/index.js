2cc711ce87101772d09a4ac7484254c4559c6669
Jaka Ahlin <jakaahlin0>
2024-07-22 21:54:48 +0200

    cors back-end

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
+const cors=require("cors")
 dotenv.config()
 
 const port = 5000
 const users = require("./routes/users.js")
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}));
+app.use(cors({
+  methods:["GET", "POST"],
+}))
+
+
 
 app.get("/",(req,res)=>{
 res.send("hola")
