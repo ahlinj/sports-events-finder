919c41bfa6c238096543013d1d2ba8b7fce0e6c7
Jaka <jakaahlin0@gmail.com>
2024-07-30 15:55:27 +0200

    added signup redirection links

--- front-end/src/CustomComponents/NotLoggedInView.js
+++ front-end/src/CustomComponents/NotLoggedInView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ORGLOGIN,LOGIN} from "../Utils/Constants.js"
+import {ORGLOGIN,LOGIN, SIGNUP, ORGSIGNUP} from "../Utils/Constants.js"
 
 class NotLoggedInView extends React.Component
 {
@@ -9,6 +9,8 @@ class NotLoggedInView extends React.Component
     <div className="card" style={{margin:"10px"}}>
       <div className="card-body">
         <h5 className="card-title">Welcome!!!</h5>
+      </div>
+      <div className="card" style={{margin:"10px"}}>
         <p className="card-text">Please login to view this page.</p>
         <p>
             Click <a onClick={() => this.props.setView({ page: LOGIN })} href="#" >here</a> to login as user.
@@ -16,6 +18,16 @@ class NotLoggedInView extends React.Component
         
         <p>
             Click <a onClick={() => this.props.setView({ page: ORGLOGIN })} href="#" >here</a> to login as organization.
+        </p>
+      </div>
+      <div className="card" style={{margin:"10px"}}>
+        <p className="card-text">If you don't have an account please signup.</p>
+        <p>
+            Click <a onClick={() => this.props.setView({ page: SIGNUP })} href="#" >here</a> to signup as user.
+        </p>
+        
+        <p>
+            Click <a onClick={() => this.props.setView({ page: ORGSIGNUP })} href="#" >here</a> to signup as organization.
         </p>
       </div>
     </div> 
