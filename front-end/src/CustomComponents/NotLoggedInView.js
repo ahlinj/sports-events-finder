e0f9806edb22871f9fbdd832cfc4e7996aa5c40c
Jaka <jakaahlin0@gmail.com>
2024-07-30 16:23:59 +0200

    NotLoggedInView page styling

--- front-end/src/CustomComponents/NotLoggedInView.js
+++ front-end/src/CustomComponents/NotLoggedInView.js
@@ -6,27 +6,27 @@ class NotLoggedInView extends React.Component
   render()
   {
     return(
-    <div className="card" style={{margin:"10px"}}>
-      <div className="card-body">
-        <h5 className="card-title">Welcome!!!</h5>
+    <div className="card" style={{ width: "80%", margin: "20px auto", border: "none"}}>
+      <div className="card" style={{margin:"10px"}}>
+        <h2 className="card-header">Welcome</h2>
       </div>
       <div className="card" style={{margin:"10px"}}>
-        <p className="card-text">Please login to view this page.</p>
-        <p>
+        <h6 className="card-header">Please login to view this page.</h6>
+        <p style={{margin:"10px"}}>
             Click <a onClick={() => this.props.setView({ page: LOGIN })} href="#" >here</a> to login as user.
-        </p>
+        </p >
         
-        <p>
+        <p style={{margin:"10px"}}>
             Click <a onClick={() => this.props.setView({ page: ORGLOGIN })} href="#" >here</a> to login as organization.
         </p>
       </div>
       <div className="card" style={{margin:"10px"}}>
-        <p className="card-text">If you don't have an account please signup.</p>
-        <p>
+        <h6 className="card-header">If you don't have an account please signup.</h6>
+        <p style={{margin:"10px"}}>
             Click <a onClick={() => this.props.setView({ page: SIGNUP })} href="#" >here</a> to signup as user.
         </p>
         
-        <p>
+        <p style={{margin:"10px"}}>
             Click <a onClick={() => this.props.setView({ page: ORGSIGNUP })} href="#" >here</a> to signup as organization.
         </p>
       </div>
