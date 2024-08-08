8c73d88e3cb346486d4c3e6746715b48c1060339
Jaka <jakaahlin0@gmail.com>
2024-08-08 15:57:53 +0200

    display all tokens by person FE

--- front-end/src/CustomComponents/TokensView.js
+++ front-end/src/CustomComponents/TokensView.js
@@ -7,12 +7,14 @@ class TokensView extends React.Component
     constructor(props) {
         super(props)
         this.state = {
-            totalTokens: ''
+            totalTokens: '',
+            tokens: []
         }
       }
 
       componentDidMount() {
-        this.fetchTokenTotal();
+        this.fetchTokenTotal()
+        this.fetchAllTokensByPerson()
       }
           
 
@@ -33,11 +35,25 @@ class TokensView extends React.Component
           })
       }
 
+      fetchAllTokensByPerson = () => {
+        axios.post(API_URL+'/tokens/all',{
+            name:this.props.user
+          })
+          .then(res=>{
+            console.log("Received all tokens from server...", res.data);
+            this.setState({
+                tokens: res.data
+              })
+          })
+          .catch(err=>{
+            console.log(err)
+          })
+      }
 
     render()
     {
       return(
-      <div className="card" style={{ width: "80%", margin: "20px auto", border: "none"}}>
+      <div className="card" style={{ width: "80%", margin: "20px auto"}}>
         <div className="card" style={{margin:"10px"}}>
           <h2 className="card-header">Tokens</h2>
         </div>
@@ -47,11 +63,44 @@ class TokensView extends React.Component
             {this.state.totalTokens}
           </p >
         </div>
+        <table className="table" >
+              <thead>
+                <tr>
+                  <th>Event Name</th>
+                  <th>Date Received</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+              {this.state.tokens.map(tok => {
+                return (
+                  <tr key={tok.id}>
+                    <td>{tok.dogodekIme}</td>
+                    <td>{formatDateTime(tok.datumPrejetja)}</td>
+                    <td>{tok.vrednost}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+            </table>
         
 
       </div> 
       )
     }
 }
+
+function formatDateTime(dateTimeString) {
+    const date = new Date(dateTimeString);
+  
+    const day = date.getUTCDate().toString().padStart(2, '0');
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, '0'); 
+    const year = date.getUTCFullYear();
+  
+    const hours = date.getUTCHours().toString().padStart(2, '0');
+    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+  
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+  }
 
 export default TokensView
