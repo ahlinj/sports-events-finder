8fc106ec16a796f294d1afa35a5cc9be3c40e804
Jaka <jakaahlin0@gmail.com>
2024-08-09 16:13:26 +0200

    added current token leaderboard in prizes FE

--- front-end/src/CustomComponents/PrizesView.js
+++ front-end/src/CustomComponents/PrizesView.js
@@ -1,16 +1,58 @@
 import React from 'react'
+import axios from "axios"
+import { API_URL } from "../Utils/Configuration.js"
 
 class PrizesView extends React.Component
 {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tokens: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchTokens();
+  }
+
+  fetchTokens = () => {
+    axios.get(API_URL+'/prizes')
+      .then(res => {
+        this.setState({
+          tokens: res.data
+        });
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+
   render()
   {
     return(
-    <div className="card" style={{margin:"10px"}}>
-      <div className="card-body">
-        <h5 className="card-title">Welcome!!!</h5>
-        <p className="card-text">You are in the prizes page</p>
+      <div className="card" style={{ width: "80%", margin: "20px auto" }}>
+        <h2 className="card-header">Current leaderboard</h2>
+        <div className="card-body">
+           
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Username</th>
+                  <th>Total tokens</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.tokens.map(tok => (
+                  <tr>
+                    <td>{tok.username}</td>
+                    <td>{tok.total_vrednost}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+        </div>
       </div>
-    </div> 
     )
   }
 }
