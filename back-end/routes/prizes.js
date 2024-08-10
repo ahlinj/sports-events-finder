f8b4fde28dc429b9e408c0bddc13a68de03978b3
Jaka Ahlin <jakaahlin0>
2024-08-10 23:29:45 +0200

    ability to add prizes by orgs in db BE

--- back-end/routes/prizes.js
+++ back-end/routes/prizes.js
@@ -15,5 +15,32 @@ prizes.get('/', async (req,res, next)=>{
     }
 })
 
+prizes.post('/add', async (req,res, next)=>{
+    let value = req.body.value
+    let sponsor = req.body.sponsor
+
+    if (value && sponsor) 
+        {
+            try
+            {
+             let queryResult=await db.AddPrize(value,sponsor)
+             if (queryResult.affectedRows) {
+                console.log("New prize added!!")
+              }
+                   
+            }
+            catch(err){
+                console.log("Error:"+err)
+                res.sendStatus(500)
+            }    
+        }
+        else
+        {
+            console.log("A field is missing!")
+        }
+    
+        res.end(); 
+})
+
 
 module.exports = prizes
