fabbaeec505232aa07d624a7ccf7d6d9c072d42b
Jaka <jakaahlin0@gmail.com>
2024-08-11 12:59:09 +0200

    display currently available prizes FE

--- front-end/src/CustomComponents/PrizesView.js
+++ front-end/src/CustomComponents/PrizesView.js
@@ -7,12 +7,14 @@ class PrizesView extends React.Component
   constructor(props) {
     super(props)
     this.state = {
-      tokens: []
+      tokens: [],
+      prizes: []
     }
   }
 
   componentDidMount() {
-    this.fetchTokens();
+    this.fetchTokens()
+    this.fetchPrizes()
   }
 
   fetchTokens = () => {
@@ -27,12 +29,50 @@ class PrizesView extends React.Component
       })
   }
 
+  fetchPrizes = () => {
+    axios.get(API_URL+'/prizes/get')
+      .then(res => {
+        this.setState({
+          prizes: res.data
+        });
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+
 
   render()
   {
     return(
+      <>
       <div className="card" style={{ width: "80%", margin: "20px auto" }}>
-        <h2 className="card-header">Current leaderboard</h2>
+        <h2 className="card-header">Current available prizes</h2>
+        <div className="card-body">
+           
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Sponsor</th>
+                  <th>Publication date</th>
+                  <th>Value in €</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.prizes.map(prz => (
+                  <tr>
+                    <td>{prz.sponzor}</td>
+                    <td>{formatDateTime(prz.datumObjave)}</td>
+                    <td>{prz.vsota}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+        </div>
+      </div>
+      <div className="card" style={{ width: "80%", margin: "20px auto" }}>
+        <h2 className="card-header">Current tokens leaderboard</h2>
         <div className="card-body">
            
             <table className="table">
@@ -53,8 +93,23 @@ class PrizesView extends React.Component
             </table>
         </div>
       </div>
+      </>
     )
   }
+}
+
+
+function formatDateTime(dateTimeString) {
+  const date = new Date(dateTimeString);
+
+  const day = date.getUTCDate().toString().padStart(2, '0');
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, '0'); 
+  const year = date.getUTCFullYear();
+
+  const hours = date.getUTCHours().toString().padStart(2, '0');
+  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
 }
 
 export default PrizesView
