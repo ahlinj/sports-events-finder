8ed236bf8f2cf3b2b512655b9376d3b7536a5ba9
Jaka <jakaahlin0@gmail.com>
2024-08-17 14:41:35 +0200

    added aboput us page text FE

--- front-end/src/CustomComponents/AboutView.js
+++ front-end/src/CustomComponents/AboutView.js
@@ -8,7 +8,11 @@ class AboutView extends React.Component
     <div className="card" style={{margin:"10px"}}>
     <div className="card-body">
       <h5 className="card-title">About us</h5>
-      <p className="card-text">Do you really want to know about us? </p>
+      <p className="card-text">This is a sports events finder website, that gives you an easy to understand user interface to find sports events of your choice.
+        If you own an organization and would like to promote your sports events please sign up and you can promote your events for free.
+        Consider also providing a prize to our users. Users are free to join any events without any fees, only requiring to sign up. 
+        By joining events you will be able to collect tokens of different value and the most active user will receive this months sponzor prizes.
+      </p>
     </div>
   </div>
     )
