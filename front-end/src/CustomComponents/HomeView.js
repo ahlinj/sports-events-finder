a2ac7fd57a80e8038486a9c0a959fbfdd9ecf444
Jaka <jakaahlin0@gmail.com>
2024-08-17 14:46:16 +0200

    fixing typo home page FE

--- front-end/src/CustomComponents/HomeView.js
+++ front-end/src/CustomComponents/HomeView.js
@@ -8,7 +8,7 @@ class HomeView extends React.Component
     <div className="card" style={{margin:"10px"}}>
       <div className="card-body">
         <h5 className="card-title">Welcome!</h5>
-        <p className="card-text">Consider signing up to our website to access all of out features.</p>
+        <p className="card-text">Consider signing up to our website to access all of our features.</p>
       </div>
     </div> 
     )
